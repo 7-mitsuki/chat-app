0d47b8d7f406a7b16ee171c61253ab799b8aa1a0
7-mitsuki <namizuki.1127@gmail.com>
2023-12-07 16:34:22 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,6 +5,19 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def room_params
+    # 配列に対して保存を許可したい場合は、キーをシンボル引数名として、空の配列値として記述する
+    params.require(:room).permit(:name, user_ids: [])
   end
 
 end
